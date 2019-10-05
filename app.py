e2dafedad0158953287ca4d0b0b539682a9dc93f
Cao Mai <caomai@rocketmail.com>
2019-10-05 10:23:58 -0700

    Added MongoDB to store

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from pymongo import MongoClient
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.contractor_project
+cart = db.cart
+
 app = Flask(__name__)
 
-content = [
-    { "candy name": "ABCD", "candy cost": "12", "url": "some url 1"},
-    { "candy name": "CDEFG", "candy cost": "26", "url": "some url 2"}
-]
+# cart = [
+#     { "candy_name": "ABCD", "cost": "12", "url": "some url 1"},
+#     { "candy_name": "CDEFG", "cost": "26", "url": "some url 2"}
+# ]
+
+# @app.route("/")
+# def index():
+#     return render_template("home.html", msg="faslk is stpd")
 
 @app.route("/")
-def index():
-    return render_template("home.html", msg="faslk is stpd")
+def content_list():
+    return render_template("list.html", cart=cart.find())
 
 if __name__ == "__main__":
     app.run(debug=True)
