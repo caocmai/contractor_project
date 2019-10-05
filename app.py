2c37d1c03420142c84e778fa36b6409a4422a730
Cao Mai <caomai@rocketmail.com>
2019-10-05 11:17:16 -0700

    Users can create a car

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.contractor_project
-cart = db.cart
+cars = db.cars
 
 app = Flask(__name__)
 
@@ -17,8 +17,28 @@ app = Flask(__name__)
 #     return render_template("home.html", msg="faslk is stpd")
 
 @app.route("/")
-def content_list():
-    return render_template("list.html", cart=cart.find())
+def cars_list():
+    return render_template("cars_index.html", cars=cars.find())
+
+@app.route("/cars/new")
+def new_car():
+    return render_template("new_car.html")
+
+@app.route("/cars/delete_all")
+def delete_all():
+    cars.remove()
+    return redirect(url_for("cars_list"))
+
+@app.route("/new_car", methods=["POST"])
+def new_car_submit():
+    car = {
+        "model": request.form.get("model"),
+        "cost": request.form.get("cost"),
+        "url": request.form.get("url)")
+    }
+    cars.insert_one(car)
+    return redirect(url_for("cars_list"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
