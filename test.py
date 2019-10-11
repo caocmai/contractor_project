33b45ce1b2e1ddc4882bd7e798db96c4dcf41140
Cao Mai <caomai@rocketmail.com>
2019-10-10 20:20:09 -0700

    Fixed test file

--- test.py
+++ test.py
@@ -73,12 +73,12 @@ class CarTest(TestCase):
         self.assertEqual(result.status, '302 FOUND')
         mock_update.assert_called_with({'_id': sample_car_id}, {'$set': sample_car})
 
-    # @mock.patch('pymongo.collection.Collection.delete_one')
-    # def test_delete_playlist(self, mock_delete):
-    #     form_data = {'_method': 'DELETE'}
-    #     result = self.client.post(f'/cars/{sample_car_id}/delete', data=form_data)
-    #     self.assertEqual(result.status, '302 FOUND')
-    #     mock_delete.assert_called_with({'_id': sample_car_id})
+    @mock.patch('pymongo.collection.Collection.delete_one')
+    def test_delete_playlist(self, mock_delete):
+        form_data = {'_method': 'DELETE'}
+        result = self.client.post(f'/cars/{sample_car_id}/delete/', data=form_data)
+        self.assertEqual(result.status, '302 FOUND')
+        mock_delete.assert_called_with({'_id': sample_car_id})
 
 if __name__ == '__main__':
     unittest_main()
