4f4ea469241015901e952d9c8ec076b32ad70684
Cao Mai <caomai@rocketmail.com>
2019-10-15 09:07:17 -0700

    Fixed test.py

--- test.py
+++ test.py
@@ -28,20 +28,20 @@ class CarTest(TestCase):
         app.config['TESTING'] = True
 
     def test_index(self):
-        """Test the playlists homepage."""
+        """Test the car homepage."""
         result = self.client.get('/')
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Tesla Cars', result.data)
 
     def test_new(self):
-        """Test the new playlist creation page."""
+        """Test the new car creation page."""
         result = self.client.get('/cars/new')
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Model', result.data)
 
     @mock.patch('pymongo.collection.Collection.find_one')
     def test_show_car(self, mock_find):
-        """Test showing a single playlist."""
+        """Test showing a single car."""
         mock_find.return_value = sample_car
 
         result = self.client.get(f'/cars/{sample_car_id}')
@@ -50,7 +50,7 @@ class CarTest(TestCase):
         
     @mock.patch('pymongo.collection.Collection.find_one')
     def test_edit_car(self, mock_find):
-        """Test editing a single playlist."""
+        """Test editing a single car."""
         mock_find.return_value = sample_car
 
         result = self.client.get(f'/cars/edit/{sample_car_id}')
@@ -59,22 +59,22 @@ class CarTest(TestCase):
 
     @mock.patch('pymongo.collection.Collection.insert_one')
     def test_submit_car(self, mock_insert):
-        """Test submitting a new playlist."""
+        """Test submitting a new car."""
         result = self.client.post('/new_car', data=sample_car_form)
 
-        # After submitting, should redirect to that playlist's page
+        # After submitting, should redirect to that car
         self.assertEqual(result.status, '302 FOUND')
         mock_insert.assert_called_with(sample_car)
 
     @mock.patch('pymongo.collection.Collection.update_one')
-    def test_update_playlist(self, mock_update):
+    def test_update_car(self, mock_update):
         result = self.client.post(f'/cars/{sample_car_id}', data=sample_car_form)
 
         self.assertEqual(result.status, '302 FOUND')
         mock_update.assert_called_with({'_id': sample_car_id}, {'$set': sample_car})
 
     @mock.patch('pymongo.collection.Collection.delete_one')
-    def test_delete_playlist(self, mock_delete):
+    def test_delete_car(self, mock_delete):
         form_data = {'_method': 'DELETE'}
         result = self.client.post(f'/cars/{sample_car_id}/delete/', data=form_data)
         self.assertEqual(result.status, '302 FOUND')
